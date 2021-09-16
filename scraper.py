ce6ea0818881c9d882234ea9d7c89741d2ee76c4
Yash Jakhotiya <mailsforyashj@gmail.com>
2021-09-16 17:51:39 -0400

    init

---
+++ scraper.py
@@ -0,0 +1,19 @@
+import sys, time
+from selenium import webdriver
+from playsound import playsound
+
+def main():
+    url = sys.argv[1]
+    browser = webdriver.Firefox()
+    while(1):
+        browser.get(url)
+        time.sleep(5)
+        res = browser.find_elements_by_xpath("//*[contains(text(), 'Ready to meet with a recruiter?')]")
+        print(res)
+        if len(res) != 0:
+            while(1):
+                playsound('alarm.wav')
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
