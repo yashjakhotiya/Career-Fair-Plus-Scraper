adfe166827c42e1dc71e44b1704cf11aa5a1299e
Yash Jakhotiya <mailsforyashj@gmail.com>
2021-09-16 17:59:06 -0400

    change wait times

--- scraper.py
+++ scraper.py
@@ -7,13 +7,12 @@ def main():
     browser = webdriver.Firefox()
     while(1):
         browser.get(url)
-        time.sleep(5)
+        time.sleep(7)
         res = browser.find_elements_by_xpath("//*[contains(text(), 'Ready to meet with a recruiter?')]")
         print(res)
         if len(res) != 0:
             while(1):
                 playsound('alarm.wav')
-        time.sleep(5)
 
 if __name__ == "__main__":
     main()
